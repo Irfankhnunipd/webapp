bd07d6aacf0c1e0220900d2cad3fbc8af912feef
webapp <irfanullah.khan@studenti.unipd.it>
2023-03-09 21:50:03 +0100

    New class pushed

---
+++ src/main/java/demo/mvn/exmple/App.java
@@ -0,0 +1,16 @@
+package demo.mvn.exmple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Student obj = new Student(101, "irfan");
+        System.out.println(obj.rollno+" "+obj.name);
+
+    }
+}
